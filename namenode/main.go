5533b5033e9a48bf5d9a782a0fc595a884e946eb
Maxi <maxibarco.uns@gmail.com>
2025-11-18 23:14:53 +0000

    Refactorized main method

--- namenode/main.go
+++ namenode/main.go
@@ -3,25 +3,41 @@ package main
 import (
 	"dfs/utils"
 	"fmt"
+	"log"
 	"net"
 )
 
 var port int = 5000
+var logger *log.Logger
 
 func main() {
-	logger := utils.CreateLogger("NAMENODE")
-	listener, err := utils.ListenPort(port)
-	if err != 0 {
-		logger.Printf("ERROR: No se pudo escuchar en el puerto %d\n", port)
-	}
-	logger.Printf("INFO: Iniciado el servidor, escuchando el puerto %d\n", port)
+	listener := startServer()
 	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			panic(err)
-		}
+		conn := acceptConnection(listener)
 		go handleConnection(conn)
 	}
+}
+
+func startServer() net.Listener {
+	logger = utils.CreateLogger("NAMENODE")
+	listener, err := utils.ListenPort(port)
+	if err != 0 {
+		error_string := fmt.Sprintf("ERROR: No se pudo escuchar en el puerto %d\n", port)
+		logger.Print(error_string)
+		panic(error_string)
+	}
+	logger.Printf("INFO: Iniciado el servidor, escuchando el puerto %d\n", port)
+	return listener
+}
+
+func acceptConnection(listener net.Listener) net.Conn {
+	conn, err := listener.Accept()
+	if err != nil {
+		error_string := "ERROR: No se pudo aceptar la conexion\n"
+		logger.Print(error_string)
+		panic(error_string)
+	}
+	return conn
 }
 
 func handleConnection(conn net.Conn) {
