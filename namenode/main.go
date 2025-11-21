4439f2e9b7597f8ebadd65a64a0e745a9f954787
Maxi <maxibarco.uns@gmail.com>
2025-11-21 02:20:43 +0000

    Namenodes starts parsingf the system_info.json

--- namenode/main.go
+++ namenode/main.go
@@ -2,26 +2,43 @@ package main
 
 import (
 	"dfs/utils"
+	"encoding/json"
 	"log"
 	"net"
+	"os"
 )
 
 var port int = 5000
 var logger *log.Logger
-var conn net.Conn
+var systemInfo SystemInfo
 
 func main() {
-	logger = utils.CreateLogger("NAMENODE")
-	listener := utils.StartServer(port, logger)
+	listener := startNamenode()
 	for {
-		conn = utils.AcceptConnection(listener, logger)
-		go handleConnection()
+		conn := utils.AcceptConnection(listener, logger)
+		go handleConnection(conn)
 	}
 }
 
-func handleConnection() {
+func startNamenode() net.Listener {
+	logger = utils.CreateLogger("NAMENODE")
+	listener := utils.StartServer(port, logger)
+	parseSystemInfo()
+	return listener
+}
+
+func parseSystemInfo() {
+	byteValue, err := os.ReadFile("data/system_info.json")
+	if err != nil {
+		logger.Print("ERROR: No se pudo abrir el archivo de informacion del sistema.")
+		panic(err)
+	}
+	json.Unmarshal(byteValue, &systemInfo)
+}
+
+func handleConnection(conn net.Conn) {
 	buffer := make([]byte, 256)
-	commandsMap := map[string]func([]string){
+	commandsMap := map[string]func(MessageData){
 		"get": getCommand,
 		"put": putCommand,
 		"ls":  lsCommand,
@@ -34,7 +51,11 @@ func handleConnection() {
 			logger.Printf("INFO: El cliente ingreso un comando invalido: \"%s\"", message.Command)
 			utils.SendMessage(conn, "El comando ingresado no es valido.\nIngrese uno de los siguientes comandos: get, put, ls\n")
 		} else {
-			commandReaded(message.Parameters)
+			connMessage := MessageData{
+				Connection: conn,
+				Parameters: message.Parameters,
+			}
+			commandReaded(connMessage)
 		}
 	}
 }
