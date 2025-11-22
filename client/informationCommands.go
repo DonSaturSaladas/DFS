2b433397fee4f956ddd86d7b8f19e08729c2c0c0
Maxi <maxibarco.uns@gmail.com>
2025-11-22 19:45:23 +0000

    Ls command working

--- client/informationCommands.go
+++ client/informationCommands.go
@@ -6,7 +6,24 @@ import (
 )
 
 func lsCommand(data utils.Message) {
-	fmt.Print("Comando ls")
+	conn := connectToNode(namenode_ip)
+	defer conn.Close()
+	message := utils.Message{
+		Connection: conn,
+		Command:    "ls",
+	}
+	utils.SendMessage(message)
+	response := utils.ReadMessage(conn, nil)
+	if response.Command == "files" {
+		fmt.Printf("Archivos en el DFS:\n -")
+		for i, file := range response.Parameters {
+			fmt.Print(" " + file)
+			if i != len(response.Parameters)-1 {
+				fmt.Print(",")
+			}
+		}
+		fmt.Print(".\n")
+	}
 }
 func infoCommand(data utils.Message) {
 	fmt.Print("Comando info")
